1d795744173491a199a65e0c7ee8a0eeb65f7b4c
caseychester7414 <casey.chester@bryanuniversity.edu>
2020-04-19 15:03:57 -0700

    website

---
+++ week2/elements/js.js
@@ -0,0 +1,9 @@
+var newD = document.createElement("li")
+newD.textContent = "Life gets better day by day"
+var myList = document.getElementById("my-list")
+myList.prepend(newLi)
+var newP = document.createElement("p")
+newP.textContent = "All of this was created with Javascript"
+document.body.append(newP)
+var start = document.getElementById("bye")
+
---
+++ week2/website/html.js
@@ -0,0 +1,21 @@
+var hello = document.createElement("hello")
+document.body.append(hello)
+hello.textContent = "hello this is fsw-110"
+
+var nav = document.createElement("navbar")
+document.body.append(nav)
+nav.textContent ="contact us"
+
+    var newp = document.createElement("p")
+    document.body.append(newp)
+    newp.textContent = "this class so far is fun!"
+
+    
+var Alist = document.createElement("list")
+document.body.append(Alist)
+Alist.textContent = "cookies"
+
+    
+var footer = document.createElement("footer")
+document.body.append(footer)
+footer.textContent = "this is the end of the page"
